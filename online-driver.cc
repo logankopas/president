ec8d3d010944a01d45a537061c38a8904127ca1a
tkdkop <tkdkop@hotmail.com>
2013-12-23 14:23:52 -0700

    gethost() prints out host ip

--- online-driver.cc
+++ online-driver.cc
@@ -16,6 +16,7 @@
 #include <sys/fcntl.h>
 #include <ifaddrs.h>
 #include <string.h>
+#include <errno.h>
 
 #include "cards.h"
 #include "online.h"
@@ -28,6 +29,7 @@ int main(int argc, char** argv)
     (void) argv;
     (void) players;
     (void) current_deck;
+    (void) hostname;
     num_players = 0;
     
     gethost();
@@ -62,8 +64,105 @@ int main(int argc, char** argv)
     return 0;
 }*/
 
+/* 
+ * gethost() prints current host
+ * More for practice, probably won't ever call this
+ */
 int gethost()
 {
-    // create UDP socket, biind to wildcard,
-    //      connect to 1.2.3.4, getsockname
+   
+    struct addrinfo hints, *res, *p, *out, out_hints;
+    int status;
+    char ipstr[INET6_ADDRSTRLEN];
+    int s;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC; // AF_INET or AF_INET6 to force version
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    if ((status = getaddrinfo(NULL, "45555", &hints, &res)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+        return 2;
+    }
+
+    printf("IP address:\n\n");
+
+    for(p = res;p != NULL; p = p->ai_next) {
+        void *addr;
+        char *ipver;
+
+        // get the pointer to the address itself,
+        // different fields in IPv4 and IPv6:
+        if (p->ai_family == AF_INET) { // IPv4
+            struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+            addr = &(ipv4->sin_addr);
+            ipver = (char*)"IPv4";
+        } else { // IPv6
+            struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+            addr = &(ipv6->sin6_addr);
+            ipver = (char*)"IPv6";
+        }
+
+        // convert the IP to a string and print it:
+        inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+        printf("  %s: %s\n", ipver, ipstr);
+    }
+
+    s=socket(res->ai_family,SOCK_DGRAM,0);
+    if(s==-1)
+    {
+        fprintf(stderr,"socket failed\n");
+    }
+    
+    /* Set port to re-usable */
+    int yes=1;
+
+    // lose the pesky "Address already in use" error message
+   /* if (setsockopt(listener,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(int)) == -1) {
+        perror("setsockopt");
+        exit(1);
+    } */
+    
+    bind(s,res->ai_addr,res->ai_addrlen);
+    if(s==-1)
+    {
+        fprintf(stderr,"bind failed\n");
+    }
+    
+    // now connect to 1.2.3.4
+     memset(&out_hints, 0, sizeof out_hints);
+    out_hints.ai_family = AF_UNSPEC; // AF_INET or AF_INET6 to force version
+    out_hints.ai_socktype = SOCK_DGRAM;
+
+    if ((status = getaddrinfo("1.2.3.4", NULL, &out_hints, &out)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+        return 2;
+    }
+
+    if(connect(s,out->ai_addr,out->ai_addrlen)!=0)
+    {
+        fprintf(stderr, "connect failed\n");
+    }
+
+    char ip4[INET_ADDRSTRLEN];
+    sockaddr address;
+    socklen_t address_len;
+    address_len = sizeof address;
+    if(getsockname(s, &address, &address_len)!=0)
+    {
+        fprintf(stderr,"Error getting sock name\n");
+    }
+    if(address.sa_family == AF_INET)
+    {
+        inet_ntop(AF_INET,&address.sa_data,ip4,INET_ADDRSTRLEN);
+        printf("Address: %s\n",ip4);
+    }
+
+    
+    
+    freeaddrinfo(res); // free the linked list
+    freeaddrinfo(out);
+    
+    return 0;
 }
